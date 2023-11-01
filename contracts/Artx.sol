262665eac274dccedb35ddb2fa76e1ee6f77e7e2
shz-code <web.shahidul.alam@gmail.com>
2023-11-01 16:19:28 +0600

    v0.5 (Contract update)

--- contracts/Artx.sol
+++ contracts/Artx.sol
@@ -1,3 +1,11 @@
+/**
+ *   @author Shahidul Alam
+ *   @title Artx
+ *   @description Contract for Artx platform
+ *   @tags solidity, ethereum, asset transfer, digital asset buy/sell
+ *   @version v0.8.0 (Testing)
+ */
+
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
 
@@ -12,7 +20,7 @@ contract ARTX {
         string uri;
         address creator;
         address currentOwner;
-        uint64 price; // In ETH
+        uint256 price; // In ETH
         bool locked; // Represents purchaseable Art or not [True -> not purchaseable]
         uint256 creationDate;
         uint256 lastTransferDate;
@@ -30,20 +38,26 @@ contract ARTX {
         address from;
         address to;
         uint256 artId;
-        uint64 price; // In ETH
+        uint256 price; // In ETH
         string artUri;
         uint256 creationDate;
     }
     /**
      * -------------------------------------------------------------------------------
-     *   Contract Variables [user_id,art_id]
+     *   Contract Variables:
+     *       owner -> Contract owner
+     *       user_id -> Latest registered user id
+     *       art_id -> Latest uploaded art id
+     *       trans_id -> Latest occurred transaction id
      *   Mappings:
      *       users -> All Users
+     *       registered_users -> Checks for registered user using address
      *       arts -> All Arts
-     *       transactions -> All Transcations
      *       userArtsCount -> User Arts Count
+     *       transactions -> All Transactions
      * -------------------------------------------------------------------------------
      */
+    address public owner;
     uint256 internal user_id;
     uint256 internal art_id;
     uint256 internal trans_id;
@@ -53,62 +67,70 @@ contract ARTX {
     mapping(uint256 => Art) internal arts; // All Arts
     mapping(uint256 => Transaction) internal transactions; // All Transactions
     mapping(address => uint256) internal userArtsCount; // User Arts
+    mapping(address => uint256) internal userTransactionCount; // User Transactions
+
+    /**
+     * -------------------------------------------------------------------------------
+     *   Contract Constructor:
+     * -------------------------------------------------------------------------------
+     */
+
+    constructor() {
+        owner = _msgSender();
+    }
 
     /**
      * -------------------------------------------------------------------------------
      *   Contract Functions:
-     *       newArt -> Register new Art
-     *       newUser -> Register new User
-     *       getAllArts -> Get All Arts
-     *       getUserArts -> Get User Arts
-     *       unlockArt ->
-     *       lockArt ->
+     *       Art:
+     *          newArt -> Register new Art
+     *          getAllArts -> Get All Arts
+     *          getArt -> Get Specific Art
+     *          unlockArt -> Unlocks the Art for transferable
+     *          lockArt -> Locks the Art so that is is not transferable
+     *       User:
+     *          newUser -> Register new User
+     *          getAllUsers -> Get all Users
+     *          getUserArts -> Get User Arts
+     *          getUser -> Get specific user
+     *          updateUserName -> Update user name
+     *       Transaction:
+     *          getAllTransactions -> Get all transactions
+     *          getUserTransactions-> Get all user transactions
+     *          getTransaction -> Get desired transaction
+     *          transferArt -> Transfer Art from one user to another
+     *
      * -------------------------------------------------------------------------------
      */
 
+    // ==================================ART==========================================
     /**
-     *   @dev Upload new Art.
-     *        Only registered users are allowed to upload
+     *   @dev Function/newArt -> Upload new Art. Only registered users are allowed to upload
+     *   @param _uri hash of art uploaded to pinata(IPFS), @param _price Art price in ETH
      */
     function newArt(
         string memory _uri,
-        address _creator,
-        uint64 price
-    ) public onlyRegisteredUser(_creator) {
+        uint256 _price
+    ) public onlyRegisteredUser(_msgSender()) {
         Art memory newArtStruct = Art(
             art_id,
             _uri,
-            _creator,
-            _creator,
-            price * 1e18,
+            _msgSender(),
+            _msgSender(),
+            _price * 1e18,
             true,
             block.timestamp,
             block.timestamp
         );
         arts[art_id] = newArtStruct;
-        userArtsCount[_creator]++;
+        userArtsCount[_msgSender()]++;
 
         art_id++;
     }
 
     /**
-     *   @dev Register new User.
-     */
-    function newUser(string memory _name, address _addr) public {
-        User memory newUserStruct = User(
-            user_id,
-            _name,
-            _addr,
-            block.timestamp
-        );
-        users[user_id] = newUserStruct;
-        registered_users[_addr] = true;
-
-        user_id++;
-    }
-
-    /**
-     *   @dev Get All Art.
+     *   @dev Function/getAllArts -> Get All Art.
+     *   @return All Arts
      */
     function getAllArts() public view returns (Art[] memory) {
         uint256 length = getArtsLength();
@@ -120,69 +142,193 @@ contract ARTX {
     }
 
     /**
-     *   @dev Get User Art.
-     *        Filters through the arts mapping and returns all the Arts that matches the currentOwner
+     *   @dev Function/getArt -> Get Specific Art.
+     *   @param _artId Art id of desired Art
+     *   @return Desired Aty
+     */
+    function getArt(
+        uint256 _artId
+    ) public view onlyValidArt(_artId) returns (Art memory) {
+        Art memory art = arts[_artId];
+        return art;
+    }
+
+    /**
+     *   @dev Functions/unlockArt -> Unlocks the Art for transferable
+     *   @param _artId(art id)
+     */
+    function unlockArt(
+        uint256 _artId
+    )
+        public
+        onlyRegisteredUser(_msgSender())
+        onlyArtOwner(_artId, _msgSender())
+    {
+        arts[_artId].locked = false;
+    }
+
+    /**
+     *   @dev Functions/lockArt -> locks the Art so that is is not transferable
+     *   @param _artId(art id)
+     */
+    function lockArt(
+        uint256 _artId
+    )
+        public
+        onlyRegisteredUser(_msgSender())
+        onlyArtOwner(_artId, _msgSender())
+    {
+        arts[_artId].locked = true;
+    }
+
+    // ==================================USER=========================================
+
+    /**
+     *   @dev Function/newUser -> Register new User.
+     *   @param _name Name of user
+     */
+    function newUser(
+        string memory _name
+    ) public onlyAnonymousUser(_msgSender()) {
+        User memory newUserStruct = User(
+            user_id,
+            _name,
+            _msgSender(),
+            block.timestamp
+        );
+        users[user_id] = newUserStruct;
+        registered_users[_msgSender()] = true;
+
+        user_id++;
+    }
+
+    /**
+     *   @dev Function/getAllUsers -> Get All Users.
+     *   @return All Users
+     */
+    function getAllUsers() public view returns (User[] memory) {
+        uint256 length = getUsersLength();
+        User[] memory allUsers = new User[](length);
+        for (uint256 i = 0; i < length; i++) {
+            allUsers[i] = users[i];
+        }
+        return allUsers;
+    }
+
+    /**
+     *   @dev Function/getUserArts -> Get User Art. Filters through the arts mapping and returns all the Arts that matches the currentOwner
+     *   @param _addr User id of whom Arts will be fetched
+     *   @return User Arts
      */
     function getUserArts(
         address _addr
     ) public view onlyRegisteredUser(_addr) returns (Art[] memory) {
-        uint256 length = getUsersArtsCount(_addr); // Get the number of arts the user currently owns
-        Art[] memory allArts = new Art[](length);
+        uint256 userOwnedLength = getUsersArtsCount(_addr); // Get the number of arts the user currently owns
+        uint256 length = getArtsLength(); // Get total number of Arts
+        Art[] memory allArts = new Art[](userOwnedLength);
+
+        uint256 index = 0;
+
         for (uint256 i = 0; i < length; i++) {
             if (arts[i].currentOwner == _addr) {
-                allArts[i] = arts[i];
+                allArts[index] = arts[i];
+                index++;
             }
         }
         return allArts;
     }
 
     /**
-     *   @dev Functions/unlockArt -> Unlockes the Art for transferable
-     *   @param _artId(art id), _addr(user address), _userId(user id)
-     *   @return bool T
+     *   @dev Function/getUser -> Get Specific User.
+     *   @param _userId User id of whom info will be fetched
+     *   @return User info
      */
-    function unlockArt(
-        uint256 _artId,
-        address _addr
-    )
-        public
-        onlyRegisteredUser(_addr)
-        onlyArtOwner(_artId, _addr)
-        returns (bool)
-    {
-        arts[_artId].locked = false;
-        return true;
+    function getUser(
+        uint256 _userId
+    ) public view onlyValidUser(_userId) returns (User memory) {
+        User memory user = users[_userId];
+        return user;
     }
 
     /**
-     *   @dev Functions/lockArt -> lockes the Art for transferable
-     *   @param _artId(art id), _addr(user address), _userId(user id)
-     *   @return bool T
+     *   @dev Function/updateUserName -> Update name of user if he is the owner
+     *   @param _userId User id of whom name will be updated. @param _newName New name of user
      */
-    function lockArt(
-        uint256 _artId,
+    function updateUserName(
+        uint256 _userId,
+        string memory _newName
+    ) public onlyAccountOwner(_userId) {
+        users[_userId].name = _newName;
+    }
+
+    // ==================================Transaction======================================
+
+    /**
+     *   @dev Functions/getAllTransactions -> Get All Transactions.
+     *   @return All Transactions
+     */
+    function getAllTransactions() public view returns (Transaction[] memory) {
+        uint256 length = getTransactionsLength();
+        Transaction[] memory allTransactions = new Transaction[](length);
+        for (uint256 i = 0; i < length; i++) {
+            allTransactions[i] = transactions[i];
+        }
+        return allTransactions;
+    }
+
+    /**
+     *   @dev Functions/getUserTransactions -> Get User Transactions. Filters through the transactions mapping and returns all the Transactions that matches the from or to
+     *   @param _addr User address
+     *   @return All User Transactions that the user was involved
+     */
+    function getUserTransactions(
         address _addr
-    )
-        public
-        onlyRegisteredUser(_addr)
-        onlyArtOwner(_artId, _addr)
-        returns (bool)
-    {
-        arts[_artId].locked = true;
-        return true;
+    ) public view onlyRegisteredUser(_addr) returns (Transaction[] memory) {
+        uint256 userTransLength = getUserTransactionsLength(_addr); // Get the number of transactions the user currently has
+        uint256 length = getTransactionsLength(); // Get total number of Transactions
+        Transaction[] memory allTrans = new Transaction[](userTransLength);
+
+        uint256 index = 0;
+
+        for (uint256 i = 0; i < length; i++) {
+            if (transactions[i].from == _addr || transactions[i].to == _addr) {
+                allTrans[index] = transactions[i];
+                index++;
+            }
+        }
+        return allTrans;
+    }
+
+    /**
+     *   @dev Functions/getTransaction -> Get Specific Transaction.
+     *   @param _transId Transaction id
+     *   @return Desired Transaction
+     */
+    function getTransaction(
+        uint256 _transId
+    ) public view onlyValidTransaction(_transId) returns (Transaction memory) {
+        Transaction memory trans = transactions[_transId];
+        return trans;
     }
 
     /**
      *   @dev Functions/transferArt -> transfers the Art to new owner
-     *   @param _artId(art id),@param _addr(new owner address),@param _userId(new owner id)
+     *   @param _artId(art id)
+     *   @return transaction id of the current transaction
      */
     function transferArt(
-        uint256 _artId,
-        address _addr
-    ) public payable onlyRegisteredUser(_addr) onlyValidArt(_artId) {
+        uint256 _artId
+    )
+        public
+        payable
+        onlyRegisteredUser(_msgSender())
+        onlyValidArt(_artId)
+        onlyTransferableArt(_artId, _msgSender())
+        returns (uint256)
+    {
         require(
             arts[_artId].price == msg.value,
-            "The transfered fund does not match Art price"
+            "The transferred fund does not match Art price"
         );
 
         // Pay the owner
@@ -192,41 +338,87 @@ contract ARTX {
         Transaction memory newTransactionStruct = Transaction(
             trans_id,
             arts[_artId].currentOwner,
-            _addr,
+            _msgSender(),
             _artId,
             arts[_artId].price,
             arts[_artId].uri,
             block.timestamp
         );
         transactions[trans_id] = newTransactionStruct;
+
+        uint256 current_id = trans_id;
+
         trans_id++;
 
         // Update new owner info
-        arts[_artId].currentOwner = _addr;
-        arts[_artId].price = arts[_artId].price + 5;
+        userArtsCount[arts[_artId].currentOwner]--;
+        userArtsCount[_msgSender()]++;
+
+        arts[_artId].currentOwner = _msgSender();
+        arts[_artId].price = arts[_artId].price + 5 * 1e18; // Increase Art price by 5 ETH in every purchase
         arts[_artId].lastTransferDate = block.timestamp;
         arts[_artId].locked = true;
 
-        userArtsCount[arts[_artId].currentOwner]--;
-        userArtsCount[_addr]++;
+        return current_id;
     }
 
     /**
      * -------------------------------------------------------------------------------
      *   Contract Modifiers:
-     *       onlyRegisteredUser -> Only registered users accessible
+     *       onlyAnonymousUser -> Only Valid user can register
+     *       onlyRegisteredUser -> Only registered users can access [Check using address]
+     *       onlyValidUser -> Only registered users can access [Check using user id]
+     *       onlyAccountOwner -> Only user himself can access
+     *       onlyValidArt -> Check if Art exists
      *       onlyArtOwner -> Only Art owner accessible
-     *       onlyValidArt ->
+     *       onlyTransferableArt -> Only Valid Art can be accessed and transferred
+     *       onlyValidTransaction -> Only valid transaction can be accessed
      * -------------------------------------------------------------------------------
      */
 
     /**
-     *   @dev Modifier/onlyRegisteredUser -> Only Registered User on the platform can access
+     *   @dev Modifier/onlyAnonymousUser -> Only Valid user can register
+     */
+    modifier onlyAnonymousUser(address _addr) {
+        require(registered_users[_addr] == false, "User already registered");
+        _;
+    }
+
+    /**
+     *   @dev Modifier/onlyRegisteredUser -> Only Registered User on the platform can access [Check using address]
      */
     modifier onlyRegisteredUser(address _addr) {
         require(registered_users[_addr] == true, "User Must be Registered");
         _;
     }
+
+    /**
+     *   @dev Modifier/onlyValidUser -> Only Registered User on the platform can access [Check using id]
+     */
+    modifier onlyValidUser(uint256 _userId) {
+        require(users[_userId].addr != address(0), "User not found");
+        _;
+    }
+
+    /**
+     *   @dev Modifier/onlyAccountOwner -> Only user himself can access
+     */
+    modifier onlyAccountOwner(uint256 _userId) {
+        require(
+            users[_userId].addr == _msgSender(),
+            "You have no permission to change the credentials"
+        );
+        _;
+    }
+
+    /**
+     *   @dev Modifier/onlyValidArt -> Check if Art exists
+     */
+    modifier onlyValidArt(uint256 _artId) {
+        require(arts[_artId].creator != address(0), "Art doesn't exists");
+        _;
+    }
+
     /**
      *   @dev Modifier/onlyArtOwner -> Only valid Art owner can access
      */
@@ -238,14 +430,29 @@ contract ARTX {
         );
         _;
     }
+
     /**
-     *   @dev Modifier/onlyValidArt -> Only Valid Art can be accessed and transfered
+     *   @dev Modifier/onlyTransferableArt -> Only Valid Art can be accessed and transferred
      */
-    modifier onlyValidArt(uint256 _artId) {
-        require(arts[_artId].creator != address(0), "Art doesn't exists");
+    modifier onlyTransferableArt(uint256 _artId, address _addr) {
         require(
             arts[_artId].locked == false,
             "Art is not available for purchase"
+        );
+        require(
+            arts[_artId].currentOwner != _addr,
+            "User can not purchase his own Art"
+        );
+        _;
+    }
+
+    /**
+     *   @dev Modifier/onlyValidTransaction -> Only valid transaction can be accessed
+     */
+    modifier onlyValidTransaction(uint256 _transId) {
+        require(
+            transactions[_transId].from != address(0),
+            "Transaction not found"
         );
         _;
     }
@@ -253,30 +460,62 @@ contract ARTX {
     /**
      * -------------------------------------------------------------------------------
      *   Contract Utilities:
+     *       _msgSender -> Returns msg sender address
      *       getArtsLength -> Returns arts mapping length
      *       getUsersLength -> Returns users mapping length
-     *       getUsersArtsCount -> Reutrns user owned Art count
+     *       getUsersArtsCount -> Returns user owned Art count
+     *       getTransactionsLength ->
+     *       getUserTransactionsLength ->
      * -------------------------------------------------------------------------------
      */
 
     /**
-     *   @dev Return (arts mapping length == art_id).
+     *   @dev Utilities/_msgSender
+     *   @return address of msg sender.
+     */
+    function _msgSender() internal view returns (address) {
+        return msg.sender;
+    }
+
+    /**
+     *   @dev Utilities/getArtsLength
+     *   @return (arts mapping length == art_id).
      */
     function getArtsLength() internal view returns (uint256) {
         return art_id;
     }
 
     /**
-     *   @dev Return (users mapping length == user_id).
+     *   @dev Utilities/getUsersLength
+     *   @return (users mapping length == user_id).
      */
     function getUsersLength() internal view returns (uint256) {
         return user_id;
     }
 
     /**
-     *   @dev Returns Number of Arts User Currenttly Own.
+     *   @dev Utilities/getUsersArtsCount
+     *   @return user Art count
      */
     function getUsersArtsCount(address _addr) internal view returns (uint256) {
         return userArtsCount[_addr];
+    }
+
+    /**
+     *   @dev Utilities/getTransactionsLength
+     *   @return transaction mapping length
+     */
+    function getTransactionsLength() internal view returns (uint256) {
+        return trans_id;
+    }
+
+    /**
+     *   @dev Utilities/getUserTransactionsLength
+     *   @return user Transaction count
+     */
+    function getUserTransactionsLength(
+        address _addr
+    ) internal view returns (uint256) {
+        return userTransactionCount[_addr];
     }
 }
